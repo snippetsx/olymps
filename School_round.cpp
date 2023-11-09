89ea8899746062780c1d6c994a1117c80554cbd0
SnippetsX <snippetsx@iMac.local>
2023-11-09 15:58:53 +0300

    added all school round tasks to one file

--- School_round.cpp
+++ School_round.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int main()
+int b()
+{
+    int n;
+    int k;
+    int tmp;
+
+    cin >> n >> k;
+    tmp = n % k;
+    cout << min(tmp, k - tmp);
+    
+    return 0;
+}
+
+int c()
 {
     int n;
     int a;
     int b;
     int c;
+    
     cin >> n >> a >> b >> c;
     if ((a <= b && a <= c))
     {
